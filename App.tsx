0ab8b89a48989114555112ca94264152f02b7bd3
al-bur <64825713+al-bur@users.noreply.github.com>
2022-10-11 21:07:41 +0900

    2 files modified

--- App.tsx
+++ App.tsx
@@ -2,9 +2,21 @@ import * as React from 'react';
 import './style.css';
 
 export default function App() {
+  const [name, setName] = React.useState('');
+
+  const handleSetName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value.trim());
+  };
+
   return (
-    <div>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        console.log(name);
+      }}
+    >
+      <input value={name} onChange={handleSetName} pattern="[^\s]+" required />
+      <button type="submit">제출</button>
+    </form>
   );
 }
