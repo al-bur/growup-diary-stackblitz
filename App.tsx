9cf3db97432021644b4d965098606f0bc2411875
al-bur <64825713+al-bur@users.noreply.github.com>
2022-10-28 14:31:06 +0900

    2 files modified

--- App.tsx
+++ App.tsx
@@ -2,9 +2,50 @@ import * as React from 'react';
 import './style.css';
 
 export default function App() {
+  console.log('A rendering');
+
+  React.useEffect(() => {
+    console.log('A');
+  }, []);
+
+  React.useEffect(() => {
+    console.log('A-1');
+  }, []);
+
+  React.useEffect(() => {
+    console.log('A-2');
+  }, []);
+
+  React.useEffect(() => {
+    console.log('A-3');
+  }, []);
+
   return (
     <div>
-      <p>Start editing to see some magic happen :)</p>
+      <ChildComponent />
     </div>
   );
+}
+
+function ChildComponent() {
+  console.log('B rendering');
+
+  React.useEffect(() => {
+    console.log('B');
+  }, []);
+  return (
+    <div>
+      <GrandChildComponent />
+    </div>
+  );
+}
+
+function GrandChildComponent() {
+  console.log('C rendering');
+
+  React.useEffect(() => {
+    console.log('C');
+  }, []);
+
+  return <div>grandchild</div>;
 }
